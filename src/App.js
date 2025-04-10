a989a4e22440cf6aff19224228435fc916e1e9af
saiphanindra24 <saiphanindra.gampala@gmail.com>
2025-04-10 11:31:53 +0530

    setup done

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import AllProducts from "./components/AllProducts";
+import Footer from "./components/Footer";
+import Herosection from "./components/Herosection";
+import Navbar from "./components/Navbar";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Herosection />
+      <AllProducts />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
